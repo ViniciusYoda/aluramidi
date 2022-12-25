0a4e3512f6470eb961fca9e3cf8fb7cfa98382f3
Vinícius Yoda <vyoda4604@gmail.com>
2022-12-25 14:16:37 -0300

    querySelectorAll

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ document.querySelector('#som_tecla_pom').play();
 
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom();
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick = tocaSomPom;
 
 
