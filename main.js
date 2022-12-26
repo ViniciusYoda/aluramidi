455a6c2760f222d6064cfb66014ea97ec1d09615
Vinícius Yoda <vyoda4604@gmail.com>
2022-12-26 16:05:56 -0300

    projeto finalizado

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-function tocaSom(idElementoAudio) {
+function tocaSom(seletorAudio) {
 
-   document.querySelector(idElementoAudio).play();
+   const elemento = document.querySelector(seletorAudio);
+
+   if (elemento && elemento.localName === 'audio') {
+         elemento.play();
+   } else {
+      alert('Elemento não encontrado');
+   }
 
 }
 
@@ -17,6 +23,16 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
    tecla.onclick = function() {
       tocaSom(idAudio);
    };
+
+   tecla.onkeydown = function(evento) {
+      if (evento.code === 'Space' || evento.code === 'Enter') {
+         tecla.classList.add('ativa');
+      }
+   }
+
+   tecla.onkeyup = function() {
+      tecla.classList.remove('ativa')
+   }
 
 }
 
